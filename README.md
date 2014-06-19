cf601af43dc44b6b3a41d392ab495aa7fe15f048
Vinod Pandey <vpforyou@gmail.com>
2014-06-19 22:15:42 +0530

    initial version

--- README.md
+++ README.md
@@ -2,3 +2,44 @@ linux-dev-box
 =============
 
 Linux Dev Box
+
+
+Installation
+-------------
+git clone git@github.com:vinodpandey/linux-dev-box.git
+cd linux-dev-box
+vagrant up
+vagrant ssh
+
+
+Softwares Included
+-------------------
+1. MySQL & mysql-devel
+   Root password: vagrant
+   sudo /etc/init.ht/mysql restart
+2. Apache 
+   URL: http://192.168.33.10/
+   Location: /var/www/html
+   Log: /etc/httpd/logs (error_log, access_log)
+   sudo /etc/init.d/httpd restart
+3. Git
+4. phpMyadmin
+   URL: http://192.168.33.10/phpmyadmin/
+   Credentials: root/vagrant
+    
+
+
+
+
+
+
+puppetlabs-stdlib
+puppetlabs-concat
+puppetlabs/firewall
+
+puppet module install puppetlabs-mysql --force --modulepath '/vagrant/code/puppet/modules'
+puppet module install puppetlabs-stdlib --force --modulepath '/vagrant/code/puppet/modules'
+puppet module install puppetlabs-apache --force --modulepath '/vagrant/code/puppet/modules'
+puppet module install puppetlabs-concat --force --modulepath '/vagrant/code/puppet/modules'
+puppet module install thias-php --force --modulepath '/vagrant/code/puppet/modules'
+puppet module install puppetlabs/firewall --force --modulepath '/vagrant/code/puppet/modules'
---
+++ puppet/manifests/init.pp
@@ -0,0 +1,63 @@
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+
+exec { "grab-epel":
+		command => "/bin/rpm -Uvh /vagrant/code/puppet/conf/epel-release-6-8.noarch.rpm",
+		creates => "/etc/yum.repos.d/epel.repo",
+		alias   => "grab-epel",
+}
+
+# mysql
+class { '::mysql::server':
+  root_password    => 'vagrant'
+}
+
+# apache
+class {'apache':}
+apache::mod { 'proxy': }
+apache::mod { 'proxy_http': }
+include apache::mod::php
+
+#php
+php::ini { '/etc/php.ini':
+  display_errors => 'On',
+  memory_limit   => '256M',
+}
+php::ini { '/etc/httpd/conf/php.ini':
+  mail_add_x_header => 'Off',
+  # For the parent directory
+  require => Package['httpd'],
+}
+
+class { 'php::mod_php5': inifile => '/etc/httpd/conf/php.ini' }
+
+
+# Ensure Git is installed
+package { "git": ensure => present }
+
+# Ensure mysql-devel present, for mysql-python in django 
+package { "mysql-devel": ensure => present }
+
+# phpMyAdmin
+class phpmyadmin {
+  package { "phpMyAdmin":
+    ensure  => present,
+    require => Exec['grab-epel'],
+  }
+
+  file { "/etc/httpd/conf.d/phpMyAdmin.conf":
+		owner   => "root",
+		group   => "root",
+		mode    => 644,
+		replace => true,
+		ensure  => present,
+		source  => "/vagrant/code/puppet/conf/phpMyAdmin.conf",
+		require => Package["httpd"],
+		notify  => Service["httpd"],
+  }
+}
+include phpmyadmin
+
+
+
+
